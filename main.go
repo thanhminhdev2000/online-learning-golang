72b3d357ab2ea5fa19f063900143a73574a3f3b6
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-10-27 22:15:33 +0700

    Accept All request *

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 	router.RedirectTrailingSlash = false
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
